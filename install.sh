034219d5ee0351dbb8f7cf43b6c4ee0c66442ea7
jandrewrfarrell <jandrewrfarrell@gmail.com>
2014-03-03 14:17:48 -0500

    update install

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
-g++ ./src/ModelDist2.cpp -o ./bin/ModelDist2 -std=gnu++0x -fopenmp
-g++ ./src/Overlap19.cpp -o ./bin/Overlap19 -std=gnu++0x -fopenmp
-g++ ./src/OverlapRegion2.cpp -o ./bin/OverlapRegion2 -std=gnu++0x -fopenmp
-g++ ./src/RUFUSv5.Filter.cpp -o ./bin/RUFUSv5.Filter -std=gnu++0x -fopenmp
-g++ ./src/RUFUSv6.BuildHash.cpp -o ./bin/RUFUSv6.BuildHash -std=gnu++0x -fopenmp
-cd ./bin/jellyfish-2.0.0/
-
-path=$( pwd )
-echo "path=$path"
-./configure --prefix=$path
-make 
-make install
-cd ../
+g++ ModelDist2.cpp -o ModelDist2 -std=gnu++0x -fopenmp
+g++ Overlap19.cpp -o Overlap19 -std=gnu++0x -fopenmp
+g++ OverlapRegion2.cpp -o OverlapRegion2 -std=gnu++0x -fopenmp
+g++ RUFUSv5.Filter.cpp -o RUFUSv5.Filter -std=gnu++0x -fopenmp
+g++ RUFUSv6.BuildHash.cpp -o RUFUSv6.BuildHash -std=gnu++0x -fopenmp
 
