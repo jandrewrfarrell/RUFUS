98662c102d68f260571df1c1a9ecfc2048cf9945
John Farrell <u0991464@ono.wasatch.peaks>
2016-04-14 10:45:40 -0600

    fixed bugs in install script

--- Install.sh
+++ Install.sh
@@ -1,16 +1,18 @@
 echo "update script paths"
-perl -p -i -e "s/RDIR=.*\n/RDIR=$( echo $(pwd)| perl -p -i -e "s/\//\\\\\//g")/g" scripts/RunRUFUS.1000G.withDupRemoce.sh 
-perl -p -i -e "s/RDIR=.*\n/RDIR=$( echo $(pwd)| perl -p -i -e "s/\//\\\\\//g")/g"  scripts/OverlapBashMultiThread.sh 
+perl -p -e "s/RDIR=.*\n/RDIR=$( echo $(pwd)| perl -p -e "s/\//\\\\\//g")\n/g" scripts/RunRUFUS.1000G.withDupRemoce.sh
+perl -p -e "s/RDIR=.*\n/RDIR=$( echo $(pwd)| perl -p -e "s/\//\\\\\//g")\n/g"  scripts/OverlapBashMultiThread.sh
+
+RUFUS_DIR=$(pwd)
 
 echo "bulding rufus executables"
 g++ src/AnnotateOverlap.cpp -o bin/AnnotateOverlap -std=gnu++0x
-g++ src/ConvertFASTqD.to.FASTQ.cpp -o bin/ConvertFASTqD.to.FASTQ 
-g++ src/ModelDist.cpp -o bin/ModelDist -fopenmp 
-g++ src/Overlap.cpp -o bin/Overlap -std=gnu++0x -fopenmp 
-g++ src/OverlapRegion.cpp -o bin/OverlapRegion -std=gnu++0x -fopenmp 
+g++ src/ConvertFASTqD.to.FASTQ.cpp -o bin/ConvertFASTqD.to.FASTQ
+g++ src/ModelDist.cpp -o bin/ModelDist -fopenmp
+g++ src/Overlap.cpp -o bin/Overlap -std=gnu++0x -fopenmp
+g++ src/OverlapRegion.cpp -o bin/OverlapRegion -std=gnu++0x -fopenmp
 g++ src/ReplaceQwithDinFASTQD.cpp -o bin/ReplaceQwithDinFASTQD
-g++ ./src/RUFUS.Filter.cpp -o ./bin/RUFUS.Filter -std=gnu++0x -fopenmp 
-g++ src/RUFUS.Build.cpp -o bin/RUFUS.Build 
+g++ ./src/RUFUS.Filter.cpp -o ./bin/RUFUS.Filter -std=gnu++0x -fopenmp
+g++ src/RUFUS.Build.cpp -o bin/RUFUS.Build
 g++ ./src/RUFUS.interpret.cpp ./src/include/* -o ./bin/RUFUS.interpret -std=gnu++0x
 
 
@@ -18,37 +20,37 @@ echo "bulding external programs"
 cd src/externals/
 
 if [ -e ./jellyfish-2.2.5/bin/jellyfish ]
-then 
-	echo "jellyfish already installed: skipping"
+then
+        echo "jellyfish already installed: skipping"
 else
 
-	tar -xvf jellyfish-2.2.5.tar.gz 
-	cd jellyfish-2.2.5
-	mkdir bin
-	./configure --prefix=/uufs/chpc.utah.edu/common/home/u0991464/d1/home/farrelac/bin/RUFUS_git/RUFUS/src/externals/jellyfish-2.2.5/bin
-	make
-	make install
-	cd ..
-fi 
+        tar -xvf jellyfish-2.2.5.tar.gz
+        cd jellyfish-2.2.5
+        mkdir bin
+        ./configure --prefix=$RUFUS_DIR/src/externals/jellyfish-2.2.5/bin
+        make
+        make install
+        cd ..
+fi
 
 if [ -e ./gkno_launcher/gkno ]
-then 
-	echo "know already built: skipping"
+then
+        echo "know already built: skipping"
 else
-	git clone https://github.com/gkno/gkno_launcher.git
-	cd gkno_launcher/
-	./gkno build
-	cd ..	
-fi 
+        git clone https://github.com/gkno/gkno_launcher.git
+        cd gkno_launcher/
+        ./gkno build
+        cd ..
+fi
 
 if [ -e ./gkno_launcher/resources/homo_sapiens/build_37_version_3/human_reference_v37_decoys.fa ]
-then 
-	echo "human reference paramaters set already downloaded: skipping"
+then
+        echo "human reference paramaters set already downloaded: skipping"
 else
-	cd gkno_launcher
-	./gkno add-resource human
-	./gkno bwa-index -r ./resources/homo_sapiens/build_37_version_3/human_reference_v37_decoys.fa -x ./resources/homo_sapiens/build_37_version_3/human_reference_v37_decoys
-	cd ..
-fi 
+        cd gkno_launcher
+        ./gkno add-resource human
+        ./gkno bwa-index -r ./resources/homo_sapiens/build_37_version_3/human_reference_v37_decoys.fa -x ./resources/homo_sapiens/build_37_version_3/human_reference_v37_decoys
+        cd ..
+fi
 
 cd ../../../
