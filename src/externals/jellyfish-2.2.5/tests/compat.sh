c5cc349f8f3208a4435da72e38c2ef374a19f56a
JAndrewRFarrell <jandrewrfarrell@gmail.com>
2016-04-15 05:11:37 +0000

    testing push from amazon

---
+++ src/externals/jellyfish-2.2.5/tests/compat.sh
@@ -0,0 +1,27 @@
+if [ -z "$nCPUs" ]; then
+    nCPUs=$(grep -c '^processor' /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+fi
+pref=$(basename $0 .sh)
+DIR=../bin
+JF="$DIR/jellyfish"
+[ -n "$VALGRIND" ] && JF="valgrind $JF"
+SRCDIR=/opt/rufus/RUFUS/src/externals/jellyfish-2.2.5
+BUILDDIR=/opt/rufus/RUFUS/src/externals/jellyfish-2.2.5
+
+check () {
+    cut -d\  -f 2 $1 | xargs md5sum | sort -k2,2 | diff -w $DIFFFLAGS $1 -
+}
+
+ENABLE_RUBY_BINDING=""
+RUBY=""
+ENABLE_PYTHON_BINDING=""
+PYTHON=""
+ENABLE_PERL_BINDING=""
+PERL=""
+
+if [ -n "$DEBUG" ]; then
+    set -x;
+    DIFFFLAGS="-y"
+fi
+
+set -e
