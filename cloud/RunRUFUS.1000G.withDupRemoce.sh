f29f9ad48bf6e77f529e137711a1a9bf5cf0ed6c
jandrewrfarrell <jandrewrfarrell@gmail.com>
2016-04-25 04:55:44 +0000

    updating scripts for cloud

--- cloud/RunRUFUS.1000G.withDupRemoce.sh
+++ cloud/RunRUFUS.1000G.withDupRemoce.sh
@@ -31,6 +31,14 @@ RUFUSOverlap=$RDIR/scripts/OverlapBashMultiThread.sh
 DeDupDump=$RDIR/scripts/HumanDedup.grenrator.tenplate
 PullSampleHashes=$RDIR/cloud/CheckJellyHashList.sh
 RUFUS1kgFilter=$RDIR/bin/RUFUS.1kg.filter
+RunJelly=$RDIR/cloud/RunJellyForRUFUS
+
+
+/usr/bin/time -v bash $RunJelly $Parent1Generator $K $(echo $Threads -1 | bc)
+/usr/bin/time -v bash $RunJelly $Parent2Generator $K $(echo $Threads -1 | bc)
+/usr/bin/time -v bash $RunJelly $SiblingGenerator $K $(echo $Threads -1 | bc)
+/usr/bin/time -v bash $RunJelly $ProbandGenerator $K $(echo $Threads -1 | bc)
+
 
 perl -ni -e 's/ /\t/;print' $ProbandGenerator.Jhash.histo
 perl -ni -e 's/ /\t/;print' $Parent1Generator.Jhash.histo
@@ -68,7 +76,7 @@ then
         echo "Skipping build"
 else
 	echo "wellshit"
-	#/usr/bin/time -v ../RUFUS/bin/jellyfish-MODIFIED/bin/jellyfish merge $Parent1Generator.Jhash  $Parent2Generator.Jhash $SiblingGenerator.Jhash $ProbandGenerator.Jhash >  Family.Unique.HashList
+	/usr/bin/time -v ../RUFUS/bin/jellyfish-MODIFIED/bin/jellyfish merge $Parent1Generator.Jhash  $Parent2Generator.Jhash $SiblingGenerator.Jhash $ProbandGenerator.Jhash >  Family.Unique.HashList
 fi
 
 echo "Mut cov = $MutantMinCov and SiblingMinCov = $SiblingMinCov"
@@ -102,9 +110,6 @@ else
          /usr/bin/time -v  ../RUFUS/cloud/RUFUS.search.1kg -hf <(awk '{print $1 "\t" $2}' $SiblingGenerator.k$K_c$SiblingMinCov.HashList.prefilter ) -o $SiblingGenerator.k$K_c$SiblingMinCov.HashList  -c $RDIR/cloud/1000G.RUFUSreference.sorted.min45.tab -hs 25
 fi
 
-
-	
-
 echo "done with RUFUS build "
 
 echo "startin RUFUS filter"
@@ -113,17 +118,19 @@ then
 	echo "skipping filter"
 else 
 	rm  $ProbandGenerator.temp
-#	mkfifo $ProbandGenerator.temp
-	#/usr/bin/time -v  bash $ProbandGenerator >  $ProbandGenerator.temp &
-	#/usr/bin/time -v   $RUFUSfilter  $ProbandGenerator.k$K_c$MutantMinCov.HashList $ProbandGenerator.temp $ProbandGenerator $K 5 5 10 $Threads &
+	mkfifo $ProbandGenerator.temp
+	/usr/bin/time -v  bash $ProbandGenerator >  $ProbandGenerator.temp &
+	/usr/bin/time -v   $RUFUSfilter  $ProbandGenerator.k$K_c$MutantMinCov.HashList $ProbandGenerator.temp $ProbandGenerator $K 5 5 10 $(echo $Threads -1 | bc) &
 wait
 fi 
 
-
-echo "startin RUFUS overlap"
-/usr/bin/time -v bash $RUFUSOverlap $ProbandGenerator.Mutations.fastq 5 $ProbandGenerator $ProbandGenerator.k$MutantMinCov.HashList $Threads 
-echo "done with everything "
-
+if [ -e $ProbandGenerator.V2.overlap.hashcount.fastq.bam.vcf ]
+then 
+	echo "skipping overlap"
+else
+	echo "startin RUFUS overlap"
+	/usr/bin/time -v bash $RUFUSOverlap $ProbandGenerator.Mutations.fastq 5 $ProbandGenerator $ProbandGenerator.k$MutantMinCov.HashList $Threads $ProbandGenerator.Jhash $SiblingGenerator.Jhash $Parent1Generator.Jhash $Parent2Generator.Jhash 
+fi
 
 echo "startin RUFUS filter"
 if [ -e $SiblingGenerator.Mutations.fastq ]
@@ -131,13 +138,22 @@ then
 	echo "skipping filter" 
 else
 	rm  $SiblingGenerator.temp
-	#mkfifo $SiblingGenerator.temp
-	#/usr/bin/time -v  bash $SiblingGenerator >  $SiblingGenerator.temp &
-	#/usr/bin/time -v   $RUFUSfilter  $SiblingGenerator.k$K_c$SiblingMinCov.HashList $SiblingGenerator.temp $SiblingGenerator $K 5 5 10 $Threads &
+	mkfifo $SiblingGenerator.temp
+	/usr/bin/time -v  bash $SiblingGenerator >  $SiblingGenerator.temp &
+	/usr/bin/time -v   $RUFUSfilter  $SiblingGenerator.k$K_c$SiblingMinCov.HashList $SiblingGenerator.temp $SiblingGenerator $K 5 5 10 $(echo $Threads -1 | bc) &
 wait
-fi 
-echo "startin RUFUS overlap"
-	/usr/bin/time -v bash $RUFUSOverlap $SiblingGenerator.Mutations.fastq 5 $SiblingGenerator $SiblingGenerator.k$SiblingMinCov.HashList $Threads
+fi
+
+if [ -e $ProbandGenerator.V2.overlap.hashcount.fastq.bam.vcf ]
+then
+	echo "skipping overlap"
+else
+	echo "startin RUFUS overlap"
+	/usr/bin/time -v bash $RUFUSOverlap $SiblingGenerator.Mutations.fastq 5 $SiblingGenerator $SiblingGenerator.k$SiblingMinCov.HashList $Threads $SiblingGenerator.Jhash $ProbandGenerator.Jhash $Parent1Generator.Jhash $Parent2Generator.Jhash
+
+fi
+
+
 echo "done with everything "
 
 
