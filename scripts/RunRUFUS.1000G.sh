fba67772ea84540a71f3e9f5eb2c4ce18aa8bda9
John Farrell <u0991464@ono.wasatch.peaks>
2017-03-07 09:52:11 -0700

    fixing 1000g script

--- scripts/RunRUFUS.1000G.sh
+++ scripts/RunRUFUS.1000G.sh
@@ -55,15 +55,18 @@ MutantMinCov=$(head -2 $ProbandGenerator.Jhash.histo.7.7.model | tail -1 )
 date
 echo "starting RUFUS build "
 let "Max= $MutantMinCov*100"
-if [ -e "$Out.Family.Unique.HashList" ]
+if [ -e " $ProbandGenerator.k$MutantMinCov.HashList" ]
 then
         echo "Skipping build"
 else
-#	/usr/bin/time -v $RUFUSbuild  -c /scratch/ucgd/lustre/u0991464/RUFUS.1000g.reference/1000G.RUFUSreference.sorted.min45.tab -s $ProbandGenerator.Jhash.sorted.min2.tab -o $ProbandGenerator.HashList -hs $K -mS $MutantMinCov -max 300 -t $Threads -d ' ' -mC 0
-/usr/bin/time -v $RUFUSbuild  -c  /uufs/chpc.utah.edu/common/home/marth-d1/home/farrelac/1000gRUFUSreference/1000G.RUFUSreference.sorted.min45.tab -s $ProbandGenerator.Jhash.sorted.min2.tab -o $ProbandGenerator.HashList -hs $K -mS $MutantMinCov -max 300 -t $Threads -d ' ' -mC 0
+######use this one kingspeak##########
+#	/usr/bin/time -v $RUFUSbuild  -c /scratch/ucgd/lustre/u0991464/RUFUS.1000g.reference/1000G.RUFUSreference.sorted.min45.tab -s $ProbandGenerator.Jhash.sorted.min2.tab -o  $ProbandGenerator.k$MutantMinCov.HashList -hs $K -mS $MutantMinCov -max 300 -t $Threads -d ' ' -mC 0
+
+######use ths one on ono puhi humu and lonepeak#######
+/usr/bin/time -v $RUFUSbuild  -c  /uufs/chpc.utah.edu/common/home/marth-d1/home/farrelac/1000gRUFUSreference/1000G.RUFUSreference.sorted.min45.tab -s $ProbandGenerator.Jhash.sorted.min2.tab -o  $ProbandGenerator.k$MutantMinCov.HashList -hs $K -mS $MutantMinCov -max 300 -t $Threads -d ' ' -mC 0
 fi
 
-echo "startin RUFUS filter"
+echo "starting RUFUS filter"
 if [ -e $ProbandGenerator.Mutations.fastq ]
 then 
 	echo "skipping filter"
@@ -71,16 +74,18 @@ else
 	rm  $ProbandGenerator.temp
 	mkfifo $ProbandGenerator.temp
 	/usr/bin/time -v  bash $ProbandGenerator | $RDIR/cloud/PassThroughSamCheck $ProbandGenerator.filter.chr >  $ProbandGenerator.temp &
-	/usr/bin/time -v   $RUFUSfilter  $ProbandGenerator.HashList $ProbandGenerator.temp $ProbandGenerator $K 5 5 10 $(echo $Threads -2 | bc) &
+	/usr/bin/time -v   $RUFUSfilter   $ProbandGenerator.k$MutantMinCov.HashList $ProbandGenerator.temp $ProbandGenerator $K 5 5 10 $(echo $Threads -2 | bc) &
 	wait
 
 fi 
+
 
 if [ -e $ProbandGenerator.V2.overlap.hashcount.fastq.bam.vcf.runanyway ]
 then 
 	echo "skipping overlap"
 else
 	echo "startin RUFUS overlap"
+#	/usr/bin/time -v bash $RUFUSOverlap $ProbandGenerator.Mutations.fastq 5 $ProbandGenerator $ProbandGenerator.HashList $Threads  
 	/usr/bin/time -v bash $RUFUSOverlap $ProbandGenerator.Mutations.fastq 5 $ProbandGenerator $ProbandGenerator.k$MutantMinCov.HashList $Threads  
 fi
 
