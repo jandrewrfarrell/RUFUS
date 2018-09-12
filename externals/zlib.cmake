1c3a2f4aa4c720a2140df063eab0c75d6450a571
root <root@localhost.localdomain>
2018-09-12 17:53:28 -0400

    getting zlib to link correctly

--- externals/zlib.cmake
+++ externals/zlib.cmake
@@ -1,29 +1,3 @@
-#  For more information, please see: http://software.sci.utah.edu
-# 
-#  The MIT License
-# 
-#  Copyright (c) 2015 Scientific Computing and Imaging Institute,
-#  University of Utah.
-# 
-#  
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without limitation
-#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
-#  and/or sell copies of the Software, and to permit persons to whom the
-#  Software is furnished to do so, subject to the following conditions:
-# 
-#  The above copyright notice and this permission notice shall be included
-#  in all copies or substantial portions of the Software. 
-# 
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#  DEALINGS IN THE SOFTWARE.
-
 SET_PROPERTY(DIRECTORY PROPERTY "EP_BASE" ${ep_base})
 
 # Setting up external library for ZLIB
@@ -32,8 +6,11 @@ SET(ZLIB_DIR ${CMAKE_BINARY_DIR}/externals/zlib CACHE INTERNAL "zlib project dir
 ExternalProject_Add(${ZLIB_PROJECT}
 	GIT_REPOSITORY https://github.com/madler/zlib.git
 	GIT_TAG 50893291621658f355bc5b4d450a8d06a563053d #lock in the commit id so we don't this doesn't break in the future
-    INSTALL_COMMAND ""
+    INSTALL_COMMAND "make"
     UPDATE_COMMAND ""
+    BUILD_COMMAND "./configure"
+    CONFIGURE_COMMAND ""
+    BUILD_IN_SOURCE 1
     PREFIX ${ZLIB_DIR}
     CMAKE_CACHE_ARGS
         -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
@@ -47,3 +24,4 @@ ExternalProject_Get_Property(${ZLIB_PROJECT} BINARY_DIR)
 SET(ZLIB_LIBRARY ${BINARY_DIR}/libz.a CACHE INTERNAL "ZLIB Lib")
 SET(ZLIB_LIBRARY_PATH ${BINARY_DIR} CACHE INTERNAL "ZLIB Lib Path")
 SET(ZLIB_INCLUDE ${SOURCE_DIR} ${BINARY_DIR} CACHE INTERNAL "ZLIB Include")
+
