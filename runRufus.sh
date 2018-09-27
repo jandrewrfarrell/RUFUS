b498b2c3c41b896f97c48235bbb13d35491a6092
Will Richards <richardsw2017@gmail.com>
2018-09-27 14:26:19 -0600

    fixed minimum thread insurance syntax error

--- runRufus.sh
+++ runRufus.sh
@@ -546,10 +546,10 @@ modifiedJelly=$RDIR/cloud/jellyfish-MODIFIED-merge/bin/jellyfish
 ####################__GENERATE_JHASH_FILES_FROM_JELLYFISH__#####################
 JThreads=$((Threads / 3))
 
-#if [ (($Jthreads < 3)) ]
-#then
-#        $JThreads=3
-#fi
+if [ $Jthreads -lt 3 ]
+then
+        $JThreads=3
+fi
 
 
 for parent in "${ParentGenerators[@]}"
