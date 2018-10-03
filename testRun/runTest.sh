3eed58e30971481b5654164f6eadd98955d47a37
Will Richards <richardsw2017@gmail.com>
2018-10-03 11:14:26 -0600

    adding updated test runs to repo

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -1,1 +1,1 @@
-./../runRufus.sh -s $PWD/../resources/testData/Child.bam -c $PWD/../resources/testData/Mother.bam $PWD/../resources/testData/Father.bam -k 25 -t 40 -m 8 -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa
+./../runRufus.sh -s $PWD/../resources/testData/Child.bam -c $PWD/../resources/testData/Mother.bam -c $PWD/../resources/testData/Father.bam -k 25 -t 40 -m 8 -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa
