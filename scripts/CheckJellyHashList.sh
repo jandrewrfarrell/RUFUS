001700f401d1fc82251824ea5fc90e65517e38e4
William Richards <u0401321@redwood3.int.chpc.utah.edu>
2019-02-28 12:40:37 -0700

    unhardcoded RDIR from CheckJellyHashList.sh

--- scripts/CheckJellyHashList.sh
+++ scripts/CheckJellyHashList.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-RDIR=/uufs/chpc.utah.edu/common/home/u0401321/RUFUS
-JellyFish=$RDIR/bin/externals/jellyfish/src/jellyfish_project/bin/jellyfish
+JellyFish=./../bin/externals/jellyfish/src/jellyfish_project/bin/jellyfish
 Jhash=$1
 HashList=$2
 MinCov=$3
