249eb76c8175640a9dc81f290d099a95ce169eac
John Farrell <u0991464@redwood4.int.chpc.utah.edu>
2019-03-29 13:28:15 -0600

    adding script for FASTQ support again

---
+++ scripts/FastqToSam.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl 
+
+
+use strict;
+
+sub trim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	chomp($string);
+	return $string;
+}
+
+
+open (Fastq , $ARGV[0]) || die "ERROR could not open sam file";
+
+my $l1 = "";
+my $l2 = "";
+my $l3 = "";
+my $l4 = "";
+my @temp;
+
+my $counter = 0; 
+while ($l1 = <Fastq>)
+{
+	
+	$counter = $counter+1;
+	$l2 = <Fastq>;
+	$l3 = <Fastq>;
+	$l4 = <Fastq>;
+	chomp $l1; 
+	chomp $l2; 
+	chomp $l4;
+	print "$l1	0	*	0	*	*	*	0	0	$l2	$l4	\n";
+	
+	 
+	
+}
+#Call should be GFFfile, FastaReff, SNPFilePath
+
