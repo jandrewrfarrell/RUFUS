9db42c8c676be27cb5294b560509c7b48c876703
John Farrell <u0991464@redwood4.int.chpc.utah.edu>
2020-12-16 12:01:01 -0700

    updating test

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -1,1 +1,1 @@
-./../runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40 -m 8 -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa
+./../runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa
