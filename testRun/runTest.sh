cad85179ded514b94e02fc629dc0957e5eb28fc1
John Farrell <u0991464@redwood4.int.chpc.utah.edu>
2021-02-17 12:42:32 -0700

    updates to test

--- testRun/runTest.sh
+++ testRun/runTest.sh
@@ -1,1 +1,9 @@
-./../runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa
+if [ -e ./../runRufus.sh ]
+then 
+	echo "starting test"
+else
+	echo "could not find ./../runRufus.sh, are you in the RUFUS/runTest/ directory?"
+	exit
+fi 
+
+./../runRufus.sh -s $PWD/Child.bam -c $PWD/Mother.bam -c $PWD/Father.bam -k 25 -t 40  -r $PWD/../resources/references/small_test_human_reference_v37_decoys.fa $1
